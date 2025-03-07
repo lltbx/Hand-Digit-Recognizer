bc28570ee30aee055d3ea987ad7fbf5bd3479a5f
Mitchell <71372298+lltbx@users.noreply.github.com>
2025-03-07 22:34:13 +0200

    namespace

--- Features/linear_regression.py
+++ Features/linear_regression.py
@@ -14,7 +14,10 @@ def closed_form(X, Y, lambda_factor):
         theta - (d + 1, ) NumPy array containing the weights of linear regression. Note that theta[0]
         represents the y-axis intercept of the model and therefore X[0] = 1
     """
-
+    
+    
+    theta = (X)
+    return theta
 
 
 def compute_test_error_linear(test_x, Y, theta):
